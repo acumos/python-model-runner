a07014cf3646af90476b6e7d651afc37a04cb6bb
Eric Z <ezavesky@research.att.com>
2018-07-27 17:35:34 -0500

    initial commit of skeleton project
    - basic setup instructions, runner
    - basic test emulation for stand-up server and local test
    - add robustness for startup wait
    - TODO: modulate for different packaging? (e.g. run command?)
    
    Change-Id: Icd2f41fb94c442fc0df30154ad05e566b1995a2a
    Issue-ID: ACUMOS-1454
    Signed-off-by: Eric Z <ezavesky@research.att.com>

---
+++ docs/developer-guide.rst
@@ -0,0 +1,57 @@
+.. ===============LICENSE_START=======================================================
+.. Acumos CC-BY-4.0
+.. ===================================================================================
+.. Copyright (C) 2017-2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+.. ===================================================================================
+.. This Acumos documentation file is distributed by AT&T and Tech Mahindra
+.. under the Creative Commons Attribution 4.0 International License (the "License");
+.. you may not use this file except in compliance with the License.
+.. You may obtain a copy of the License at
+..
+..      http://creativecommons.org/licenses/by/4.0
+..
+.. This file is distributed on an "AS IS" BASIS,
+.. WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+.. See the License for the specific language governing permissions and
+.. limitations under the License.
+.. ===============LICENSE_END=========================================================
+
+==========================================
+Acumos Python Model Runner Developer Guide
+==========================================
+
+Testing
+=======
+
+We use a combination of ``tox``, ``pytest``, and ``flake8`` to test
+``acumos``. Code which is not PEP8 compliant (aside from E501) will be
+considered a failing test. You can use tools like ``autopep8`` to
+“clean” your code as follows:
+
+.. code:: bash
+
+    $ pip install autopep8
+    $ cd python-model-runner
+    $ autopep8 -r --in-place --ignore E501 testing/ examples/
+
+Run tox directly:
+
+.. code:: bash
+
+    $ cd python-model-runner
+    $ export WORKSPACE=$(pwd)  # env var normally provided by Jenkins
+    $ tox
+
+You can also specify certain tox environments to test:
+
+.. code:: bash
+
+    $ tox -e py34  # only test against Python 3.4
+    $ tox -e flake8  # only lint code
+
+And finally, you can run pytest directly in your environment *(recommended starting place)*:
+
+.. code:: bash
+
+    $ pytest
+    $ pytest -s   # verbose output
---
+++ setup.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2017-2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T and Tech Mahindra
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+from os.path import dirname, abspath, join as path_join
+from setuptools import setup, find_packages
+
+
+SETUP_DIR = abspath(dirname(__file__))
+DOCS_DIR = path_join(SETUP_DIR, 'docs')
+
+with open(path_join(SETUP_DIR, 'acumos_model_runner', '_version.py')) as file:
+    globals_dict = dict()
+    exec(file.read(), globals_dict)
+    __version__ = globals_dict['__version__']
+
+
+def _long_descr():
+    '''Yields the content of documentation files for the long description'''
+    for file in ('user-guide.rst', 'developer-guide.rst', 'release-notes.rst'):
+        doc_path = path_join(DOCS_DIR, file)
+        with open(doc_path) as f:
+            yield f.read()
+
+
+setup(
+    author='Paul Triantafyllou, Eric Zavesky',
+    author_email='trianta@research.att.com, ezavesky@research.att.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    description='Acumos model runner for Python models',
+    entry_points="""
+    [console_scripts]
+    acumos-model-runner=acumos_model_runner.runner:run
+    """,
+    install_requires=['acumos>=0.7',
+                      'Flask',
+                      'gunicorn'],
+    keywords='acumos machine learning model runner server protobuf ml ai',
+    license='Apache License 2.0',
+    long_description='\n'.join(_long_descr()),
+    name='acumos-model-runner',
+    packages=find_packages(),
+    python_requires='>=3.4',
+    url='https://gerrit.acumos.org/r/gitweb?p=python-model-runner.git',
+    version=__version__,
+)
