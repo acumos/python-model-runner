701ca73e33bd887f0211e1b003fcd39d6688d4bc
Eric Z <ezavesky@research.att.com>
2018-07-27 20:08:18 -0500

    initial commit of skeleton project
    - basic setup instructions, runner, command line robustness
    - basic test emulation for stand-up server and local test
    - add robustness for startup wait
    - TODO: unit tests for downstream?
    
    Change-Id: Icd2f41fb94c442fc0df30154ad05e566b1995a2a
    Issue-ID: ACUMOS-1454
    Signed-off-by: Eric Z <ezavesky@research.att.com>

---
+++ README.rst
@@ -0,0 +1,29 @@
+.. ===============LICENSE_START=======================================================
+.. Acumos CC-BY-4.0
+.. ===================================================================================
+.. Copyright (C) 2017-2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+.. ===================================================================================
+.. This Acumos documentation file is distributed by AT&T and Tech Mahindra
+.. under the Creative Commons Attribution 4.0 International License (the "License");
+.. you may not use this file except in compliance with the License.
+.. You may obtain a copy of the License at
+..
+..      http://creativecommons.org/licenses/by/4.0
+..
+.. This file is distributed on an "AS IS" BASIS,
+.. WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+.. See the License for the specific language governing permissions and
+.. limitations under the License.
+.. ===============LICENSE_END=========================================================
+
+===================
+python-model-runner
+===================
+
+|Build Status|
+
+This repository holds the code for the Python model runner, 
+a microservice that wraps models on-boarded by Acumos users.
+
+.. |Build Status| image:: https://jenkins.acumos.org/buildStatus/icon?job=python-model-runner-tox-verify-master
+   :target: https://jenkins.acumos.org/job/python-model-runner-tox-verify-master/
---
+++ acumos_model_runner/runner.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2017-2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T and Tech Mahindra
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+# -*- coding: utf-8 -*-
+'''
+Provides a model runner application that subscribes to iris DataFrame messages and publishes Prediction messages
+'''
+import argparse
+import json
+from functools import partial
+from os import path
+import sys
+
+import requests
+from flask import Flask, request, make_response
+from google.protobuf import json_format
+
+from gunicorn.app.base import BaseApplication
+from multiprocessing import Event
+from multiprocessing.sharedctypes import Value
+
+from acumos.wrapped import load_model
+import logging
+
+
+def invoke_method(model_method, downstream, app):
+    '''Consumes and produces protobuf binary data'''
+    logger = app.logger_ext
+    content_type = "text/plain;charset=UTF-8"
+    bytes_in = request.data
+    if not bytes_in:
+        if request.form:
+            bytes_in = dict(request.form)
+        elif request.args:
+            bytes_in = dict(request.args)
+    if type(bytes_in) == dict:  # attempt to push arguments into JSON for more tolerant parsing
+        bytes_in = json.dumps(bytes_in)
+    bytes_out = None
+
+    try:
+        if app.pargs['json_io']:
+            msg_in = json_format.Parse(bytes_in, model_method.pb_input_type())  # attempt to decode JSON
+            msg_out = model_method.from_pb_msg(msg_in)
+        else:
+            msg_out = model_method.from_pb_bytes(bytes_in)  # default from-bytes method
+        if app.pargs['json_io']:
+            bytes_out = json_format.MessageToJson(msg_out.as_pb_msg())
+            content_type = "application/javascript;charset=UTF-8"
+        else:
+            bytes_out = msg_out.as_pb_bytes()
+    except json_format.ParseError as e:
+        type_input = list(model_method.pb_input_type.DESCRIPTOR.fields_by_name.keys())
+        str_reply = "[invoke_method]: Value specification error, expected  {}, {}".format(type_input, e)
+        logger.info(str_reply)
+        resp = make_response(str_reply, 400)
+    except (ValueError, TypeError) as e:
+        str_reply = "[invoke_method]: Value conversion error: {}".format(e)
+        logger.info(str_reply)
+        resp = make_response(str_reply, 400)
+
+    if bytes_out is not None:
+        resp = None
+        for url in downstream:
+            try:
+                req_response = requests.post(url, data=bytes_out)
+                if resp is None:  # save only first response from downstream list
+                    resp = make_response(req_response.content, req_response.status_code)
+                    for header_test in ['Content-Type', 'content-type']:  # test for content type to copy from downstream
+                        if header_test in req_response:
+                            content_type = req_response[header_test]
+            except Exception as e:
+                logger.warn("Failed to publish to downstream url {} : {}".format(url, e))
+        if app.pargs['no_output']:
+            resp = make_response('OK', 201)
+        else:
+            if resp is None:  # only if not received from downstream
+                resp = make_response(bytes_out, 201)
+
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Content-Type'] = content_type
+    return resp
+
+
+class StandaloneApplication(BaseApplication):
+    '''Custom gunicorn app. Modified from http://docs.gunicorn.org/en/stable/custom.html'''
+
+    def __init__(self, pargs_raw, logger=None):
+        self.pargs = self.parse_config(pargs_raw)
+        self.options = {'bind': "{}:{}".format(self.pargs['host'], self.pargs['port']),
+                        'workers': self.pargs['workers'],
+                        'timeout': self.pargs['timeout']}
+        self.logger_ext = logger if logger is not None else logging.getLogger(__name__)
+        self.logger_ext.info("[configuration]: {}".format(self.pargs))
+        self.launch_event = Event()
+        self.launch_event.clear()
+        self.launch_count = Value('i', self.pargs['workers'])
+        super().__init__()
+
+    def load_config(self):
+        config = dict([(key, value) for key, value in self.options.items()
+                       if key in self.cfg.settings and value is not None])
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return StandaloneApplication.build_app(self)
+
+    def wait_for_startup(self, time_interval=2):
+        while self.launch_count.value > 0:
+            self.launch_event.clear()
+            self.launch_event.wait(time_interval)
+            if self.logger_ext is not None:
+                self.logger_ext.info("[wait_for_startup]: Waiting for {} of {} processes to start...".format(
+                    self.launch_count.value, self.pargs['workers']))
+
+    def parse_config(self, pargs):
+        parser = argparse.ArgumentParser(
+            description="""A script to parse and instantiate an Acumos python model""",
+            # epilog="""A running example to process 10 seconds from one minute into the clip...
+            #    python transcribe.py  -i ../data/att-dtv.videohd.mp4 -t 10 -s 60
+            # """
+        )
+        submain = parser.add_argument_group('main arguments')
+        submain.add_argument("--modeldir", type=str, default='', help='specify the model directory to load')
+        submain.add_argument("--host", type=str, default='0.0.0.0', help='the host IP to serve from')
+        submain.add_argument("--port", type=int, default=3330, help='the host PORT to serve from')
+        submain.add_argument("--timeout", type=int, default=120, help='instantiation timeout (in seconds) during start-up')
+        submain.add_argument("--workers", type=int, default=2, help='number of concurrent processes to instantiate')
+        submain = parser.add_argument_group('cascade and downstream forwarding arguments')
+        submain.add_argument("--no_output", action='store_true', help='do not return output in response, only send downstream')
+        submain.add_argument("--no_downstream", action='store_true', help='ignore downstream arguments even if in runtime')
+        submain.add_argument("--json_io", action='store_true', help='(alpha) input+output rich JSON instead of protobuf')
+
+        config_defaults, _ = parser.parse_known_args()
+        config_defaults = vars(config_defaults)
+        config_defaults.update(pargs)
+        pargs = config_defaults
+        if len(pargs['modeldir']) == 0 or not path.exists(pargs['modeldir']):
+            print("Please verify `modeldir` setting and existance, no model was found.")
+            parser.print_help()
+            sys.exit(2)
+        return pargs
+
+    @staticmethod
+    def build_app(app_instance):
+        '''Builds and returns a Flask app'''
+        downstream = []
+        if path.exists('runtime.json'):
+            with open('runtime.json') as f:
+                runtime = json.load(f)  # ad-hoc way of giving the app runtime parameters
+                if not app_instance.pargs['no_downstream']:
+                    downstream = runtime['downstream']  # list of IP:port/path urls
+                    app_instance.logger_ext.info("Found downstream forward routes {}".format(downstream))
+        else:
+            app_instance.pargs['return_output'] = True
+
+        # singleton to run in Flask, other property data is associated with app_instance
+        app = Flask(__name__)
+        # refers to ./model dir in pwd. generated by helper script also in this dir
+        app_instance.model = load_model(app_instance.pargs['modeldir'])
+
+        # dynamically add handlers depending on model capabilities
+        for method_name, method in app_instance.model.methods.items():
+            handler = partial(invoke_method, model_method=method, downstream=downstream, app=app_instance)
+            url = "/{}".format(method_name)
+            app.add_url_rule(url, method_name, handler, methods=['POST', 'GET'])
+
+            # render down the input in few forms
+            typeInput = list(method.pb_input_type.DESCRIPTOR.fields_by_name.keys())
+
+            # render down the output in few forms
+            typeOutput = list(method.pb_output_type.DESCRIPTOR.fields_by_name.keys())
+            app_instance.logger_ext.warn("[build_app]: Adding route {} [input:{}, output:{}]".format(url, typeInput, typeOutput))
+        # indicate that our value is done
+        app_instance.launch_count.value -= 1
+        app_instance.launch_event.set()
+        return app
+
+
+def run(config={}):
+    StandaloneApplication(config).run()
+
+
+if __name__ == '__main__':
+    '''Main'''
+    run()
