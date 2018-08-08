51e615abdad7feee5e634e13e1d6e1638c7c3148
Paul Triantafyllou <trianta@research.att.com>
2018-08-08 13:57:52 -0400

    Implement model runner with OAS support
    - Model runner implementation split off from Acumos Python client project
    - Added support for application/json via Content-Type and Accept headers
    - Added automatic generation of OpenAPI Specification and Swagger UI
    - Added check for protobuf decode errors
    - Added model namespace for definitions to avoid future collisions
    
    Change-Id: Ic1d194aa95e876c607dfdee7ce841ee70a9427da
    Issue-ID: ACUMOS-1349,ACUMOS-1456,ACUMOS-732,ACUMOS-1307
    Signed-off-by: Paul Triantafyllou <trianta@research.att.com>

---
+++ acumos_model_runner/tests/testing_utils.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2017-2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T and Tech Mahindra
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+'''
+Provides testing utilities
+'''
+from functools import partial
+from os.path import dirname
+
+from acumos_model_runner.utils import load_data
+
+
+_TEST_DIR = dirname(__file__)
+
+load_testing_data = partial(load_data, prefix=_TEST_DIR)
