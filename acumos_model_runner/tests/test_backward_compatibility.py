92a0a2313c9eca58444edb53ccde8e837483d72a
Jonathan GAYVALLET <jonathan.gayvallet@orange.com>
2020-06-02 11:44:09 +0200

    Fix backward compatibility issue with old models
    Model from schemas older than 0.6.0 are now
    converted to 0.6.0 as soon as possible.
    
    Added tests for models made with acumos 0.6, 0.7, 0.8 and 0.9.
    
    Issue-ID: ACUMOS-4164
    Change-Id: I1d9e429cf97fb272f24448450fd876ea88a0cdcb
    Signed-off-by: Jonathan GAYVALLET <jonathan.gayvallet@orange.com>

--- acumos_model_runner/tests/test_backward_compatibility.py
+++ acumos_model_runner/tests/test_backward_compatibility.py
@@ -2,8 +2,7 @@
 # ===============LICENSE_START=======================================================
 # Acumos Apache-2.0
 # ===================================================================================
-# Copyright (C) 2017-2020 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
-# Modifications Copyright (C) 2020 Nordix Foundation.
+# Copyright (C) 2020 Orange All rights reserved.
 # ===================================================================================
 # This Acumos software file is distributed by AT&T and Tech Mahindra
 # under the Apache License, Version 2.0 (the "License");
@@ -17,4 +16,42 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ===============LICENSE_END=========================================================
-__version__ = '0.2.5'
+'''
+Provides tests for backward compatibility
+'''
+import os
+
+import pytest
+
+from runner_helper import ModelRunner
+
+MODELS_DIR = os.path.join(os.path.dirname(__file__), "data", "backward_compatible_models")
+MODELS = os.listdir(MODELS_DIR)
+
+
+@pytest.fixture(params=MODELS)
+def model(request):
+    return os.path.join(MODELS_DIR, request.param)
+
+
+def test_model_load(model):
+    with ModelRunner(model) as _:
+        pass
+
+
+def test_model_exec(model):
+    with ModelRunner(model) as runner:
+        api = runner.api
+
+    add_input = {'x': 1, 'y': 2}
+
+    resp_json = api.method('add', json=add_input)
+    assert int(resp_json['value']) == 3
+
+    resp_proto = api.method('add', proto=add_input)
+    assert resp_proto.value == 3
+
+
+if __name__ == '__main__':
+    '''Test area'''
+    pytest.main([__file__, ])
